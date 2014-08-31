6cedbcd1dff18581a5fe5dbaa8f975bf4f38f67a
Alex Bardanov <dnbard@gmail.com>
2014-08-31 22:07:22 +0300

    heroku integration

--- service/config.js
+++ service/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.NODE_PORT || 3000,
+    port: process.env.PORT || 3000 ,
     env: process.env.NODE_ENV || 'development',
     connectionString: process.env.MONGO || 'mongodb://rhianon:rhianon@paulo.mongohq.com:10087/buh-db',
     authKey: process.env.NODE_AUTH || '123321'
--- service/gulpfile.js
+++ service/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    del = require('del');;
+    del = require('del');
 
 var paths = {
     scripts: ['!./node_modules', '!./node_modules/**/*', '!./build', './**/*'],
