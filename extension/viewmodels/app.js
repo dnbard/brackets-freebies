10e03511af788093aeb5c6518ef0e5a9b9826e5b
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2014-09-02 15:48:58 +0300

    added iframe support for documents

--- extension/viewmodels/app.js
+++ extension/viewmodels/app.js
@@ -2,7 +2,8 @@ define(function(require, exports, module){
     var ko = require('../vendor/knockout'),
         _ = require('../vendor/lodash'),
         DAL = require('../dal/index'),
-        defaultPage = 'default';
+        defaultPage = 'default',
+        iframePage = 'iframe';
 
     function AppViewModel(dialog){
         var self = this;
@@ -15,12 +16,22 @@ define(function(require, exports, module){
         this.topic = ko.observable(null);
         this.documents = ko.observableArray([]);
 
+        this.document = ko.observable(null);
+
         this.selectTopic = function(topic){
+            self.page(defaultPage);
             self.topic(topic);
 
             DAL.getDocumentsByTopic(topic._id).success(function(documents){
                 self.documents(documents);
             });
+        }
+
+        this.selectDocument = function(document){
+            self.topic(null);
+            self.document(document);
+            self.page(iframePage);
+            iframeResize();
         }
 
         this.getField = function(obj, prop){
@@ -29,6 +40,12 @@ define(function(require, exports, module){
             }
 
             return obj[prop] || '';
+        }
+
+        function iframeResize(){
+            setTimeout(function(){
+                $('.iframe-document').css('height', $('.fb-page').css('height'));
+            }, 10);
         }
 
         DAL.getTopics().success(function(data){
@@ -47,6 +64,7 @@ define(function(require, exports, module){
         this.page(defaultPage);
         this.documents([]);
         this.topic(null);
+        this.document(null);
     }
 
     module.exports = AppViewModel;
