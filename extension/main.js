b7c01f9c1e4244c923a8651916aa08b953c51ea8
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2014-09-04 17:49:29 +0300

    documents layout changes

--- extension/main.js
+++ extension/main.js
@@ -8,6 +8,8 @@ define(function (require, exports, module) {
     //list of icons - http://ionicons.com/
     ExtensionUtils.loadStyleSheet(module, 'http://code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css');
 
+    ExtensionUtils.loadStyleSheet(module, 'http://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=latin');
+
     iconService.init();
     iconService.click(modalService.showHandler);
 });
